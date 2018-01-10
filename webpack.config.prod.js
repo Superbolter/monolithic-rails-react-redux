0040b2fd92580f7b68cccd2581150371cfc02e0e
Hunter Fortuin <hunter@flitti.com>
2018-01-10 11:29:57 -0500

    fix prod

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -13,7 +13,7 @@ module.exports = {
     extensions: ['.js']
   },
   plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('production')
